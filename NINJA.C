2bafad2d3559ba7c86568c7ce486d145d8e2f37d
kalaipraba <35525844+kalaipraba@users.noreply.github.com>
2018-03-02 02:19:33 -0800

    NINJA.C

---
+++ NINJA.C
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+int ninja1,ninja2,sub;
+printf("%d%d",&ninja1,&ninja2);
+sub=ninja2-ninja1;
+printf("\n difference between %d&%d=%d",ninja1,ninja2.sub);
+return0;
+}
